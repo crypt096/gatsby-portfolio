30f64597abb7ee284b33644f08cac5b70f91ac84
crypt096 <acvjetan@gmail.com>
2021-03-12 20:50:19 +0100

    Add pages & routes.

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto omnis sunt rem, eum neque tempora itaque fuga voluptate similique, commodi quas distinctio aut cumque laudantium, quod laboriosam porro quis reprehenderit. Quam dicta amet quia saepe magni harum officia beatae necessitatibus, incidunt minima similique commodi, asperiores accusantium sint, nihil temporibus aut!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto omnis sunt rem, eum neque tempora itaque fuga voluptate similique, commodi quas distinctio aut cumque laudantium, quod laboriosam porro quis reprehenderit. Quam dicta amet quia saepe magni harum officia beatae necessitatibus, incidunt minima similique commodi, asperiores accusantium sint, nihil temporibus aut!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto omnis sunt rem, eum neque tempora itaque fuga voluptate similique, commodi quas distinctio aut cumque laudantium, quod laboriosam porro quis reprehenderit. Quam dicta amet quia saepe magni harum officia beatae necessitatibus, incidunt minima similique commodi, asperiores accusantium sint, nihil temporibus aut!</p>
+    </div>
+  )
+}
