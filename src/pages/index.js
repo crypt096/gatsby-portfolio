f4aa7530f563390f0dafb68a240f9cc0d6f578a7
crypt096 <acvjetan@gmail.com>
2021-03-13 10:45:31 +0100

    Add Navbar & Links.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
 
+import Navbar from "../components/Navbar";
+
 export default function Home() {
   return (
     <section>
+      <Navbar />
       <div>
         <h2>Design</h2>
         <h3>Develop & Deploy</h3>
