f1d7c2b732e6cbe25c562bbc726a43c9ff3924da
crypt096 <acvjetan@gmail.com>
2021-03-14 09:08:56 +0100

    Fetching and listing data.

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,44 @@
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
 
-export default function Projects() {
+export default function Projects({ data }) {
+  const projects = data.allMarkdownRemark.nodes;
+
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've created</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={`/projects/${project.frontmatter.slug}`} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+// export page query
+
+export const query = graphql`
+query ProjectsPage {
+  allMarkdownRemark {
+    nodes {
+      frontmatter {
+        title
+        stack
+        slug
+      }
+      id
+    }
+  }
+}
+`
