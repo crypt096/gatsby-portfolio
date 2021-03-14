952aab5eec43a7a14c62dcf7c2bcd10630221ec2
crypt096 <acvjetan@gmail.com>
2021-03-14 11:12:07 +0100

    GraphQL query variables

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -2,18 +2,41 @@ import React from 'react'
 import Layout from '../components/Layout';
 import Img from 'gatsby-image';
 import * as styles from '../styles/project-details.module.css';
+import { graphql } from 'gatsby';
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  console.log(data)
+  const { html } = data.markdownRemark;
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter;
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>Title</h2>
-        <h3>Stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-          {/* <Img fluid={ } /> */}
+          <Img fluid={featuredImg.childImageSharp.fluid} />
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={ } /> */}
+        <div className={styles.html} dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+}
+`
