5b27c2aa531fa833a6a05fb8fc12531830067184
crypt096 <acvjetan@gmail.com>
2021-03-14 16:36:26 +0100

    homepage-text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ data }) {
     <Layout>
       <section className={styles.header}>
         <div>
-          <h2>Design</h2>
+          <h2>Design!!!</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester.</p>
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
